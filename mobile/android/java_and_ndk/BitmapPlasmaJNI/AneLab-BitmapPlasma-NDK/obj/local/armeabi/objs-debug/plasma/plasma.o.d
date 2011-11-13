60e7f8fc9b53917077a47bafde5477e11b401119
tokufxug@gmail.com <tokufxug@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2011-11-13 15:42:20 +0000

    new create
    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@133 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ mobile/android/java_and_ndk/BitmapPlasmaJNI/AneLab-BitmapPlasma-NDK/obj/local/armeabi/objs-debug/plasma/plasma.o.d
@@ -0,0 +1,110 @@
+/Applications/eclipse3.7/workspace/AneLab-BitmapPlasma-NDK/obj/local/armeabi/objs-debug/plasma/plasma.o: \
+ /Applications/eclipse3.7/workspace/AneLab-BitmapPlasma-NDK/jni/plasma.c \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/time.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/time.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/tokuyamasadao/Android/android-ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
