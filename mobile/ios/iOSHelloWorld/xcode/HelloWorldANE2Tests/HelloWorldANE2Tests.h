d0b8ce41f601a0977ab32918be9531058ebf53f7
hiiragi.as <hiiragi.as@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2011-11-19 07:26:04 +0000

    iOS HelloWorld
    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@145 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ mobile/ios/iOSHelloWorld/xcode/HelloWorldANE2Tests/HelloWorldANE2Tests.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldANE2Tests.h
+//  HelloWorldANE2Tests
+//
+//  Created by  on 11/11/09.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloWorldANE2Tests : SenTestCase
+
+@end
