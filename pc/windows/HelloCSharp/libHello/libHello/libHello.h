ae51b8f6492ae9c1c03fe87a5ea5ded04fe627fe
g.hoehoe@gmail.com <g.hoehoe@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2012-01-15 12:56:12 +0000

    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@178 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ pc/windows/HelloCSharp/libHello/libHello/libHello.h
@@ -0,0 +1,17 @@
+// libHello.h
+
+#pragma once
+#using <mscorlib.dll>
+#using "HelloLibrary.dll"
+
+using namespace System;
+using namespace System::Runtime::InteropServices;
+using namespace HelloLibrary;
+
+namespace libHello {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
