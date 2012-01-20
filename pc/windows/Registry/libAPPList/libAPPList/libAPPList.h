2f4753f9ee541bb9ebd24cd8815409d9ee2b6ff9
g.hoehoe@gmail.com <g.hoehoe@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2012-01-20 15:24:23 +0000

    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@185 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ pc/windows/Registry/libAPPList/libAPPList/libAPPList.h
@@ -0,0 +1,24 @@
+// libAPPList.h
+
+#pragma once
+
+#include <wchar.h>
+#include <windows.h>
+#include "FlashRuntimeExtensions.h"
+
+
+#using <mscorlib.dll>
+#using "AppList_CS.dll"
+
+using namespace System;
+using namespace System::Runtime::InteropServices;
+using namespace AppList_CS;
+using namespace System::Collections;
+
+namespace libAPPList {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
