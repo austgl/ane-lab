c575f50283d5789cb48dd3c47bc493bd0ae48521
g.hoehoe@gmail.com <g.hoehoe@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2012-01-28 12:05:13 +0000

    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@189 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ pc/windows/Hello/libSampleANE/libSampleANE/libSampleANE.h
@@ -0,0 +1,15 @@
+// libSampleANE.h
+#include <stdlib.h>
+#include <string.h> 
+
+#pragma once
+
+using namespace System;
+
+namespace libSampleANE {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
