32422576cfc5e94afc39fc0e7b2e89a520678ec8
g.hoehoe@gmail.com <g.hoehoe@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2012-01-28 12:07:58 +0000

    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@190 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ pc/windows/HelloCSharp/HelloLibrary/HelloLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloLibrary {
+
+    public class HelloClass {
+        
+        public String getHelloString() {
+            return "C# DLL talking 'say Hello'!";
+        }
+    }
+}
---
+++ pc/windows/HelloCSharp/libHello/libHello/libHello.h
@@ -0,0 +1,19 @@
+// libHello.h
+#include <stdlib.h>
+#include <string.h> 
+
+#pragma once
+#using <mscorlib.dll>
+#using "HelloLibrary.dll"
+
+using namespace System;
+using namespace System::Runtime::InteropServices;
+using namespace HelloLibrary;
+
+namespace libHello {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
