353759f76701c4bbdcaf543d7e2a963d5db56933
tokufxug@gmail.com <tokufxug@gmail.com@d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc>
2014-08-30 05:46:01 +0000

    Rolto アプリ呼び出しを行うANE用ネイティブライブラリ
    git-svn-id: http://ane-lab.googlecode.com/svn/trunk@245 d34a1d4b-e036-8ff9-fd6d-47291f6e9bcc

---
+++ mobile/ios/RoltoClipboard/RoltoClipboardPrintNative/RoltoClipboardPrintANETests/RoltoClipboardPrintANETests.m
@@ -0,0 +1,34 @@
+//
+//  RoltoClipboardPrintANETests.m
+//  RoltoClipboardPrintANETests
+//
+//  Created by Sadao Tokuyama on 2014/08/16.
+//  Copyright (c) 2014年 Sadao Tokuyama. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RoltoClipboardPrintANETests : XCTestCase
+
+@end
+
+@implementation RoltoClipboardPrintANETests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
